2c24e68b69affc8a51d38bf5fd8b29006631c9a1
YakultLover <rianbowgift@gmail.com>
2022-01-27 08:13:31 +0900

    Initial commit

---
+++ src/main/java/rianbowgift/maven/maven1st_project/Maven1stProjectApplication.java
@@ -0,0 +1,15 @@
+package rianbowgift.maven.maven1st_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Maven1stProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Maven1stProjectApplication.class, args);
+
+        System.out.println("hi");
+    }
+
+}
---
+++ src/test/java/rianbowgift/maven/maven1st_project/Maven1stProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package rianbowgift.maven.maven1st_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Maven1stProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
