444300d72cd2df2e78df8f971dfcfd27d04add70
YakultLover <rianbowgift@gmail.com>
2022-01-31 19:01:05 +0900

    API TEST

--- src/main/java/rianbowgift/maven/maven1st_project/controller/HomeController.java
+++ src/main/java/rianbowgift/maven/maven1st_project/controller/HomeController.java
@@ -5,19 +5,29 @@ package rianbowgift.maven.maven1st_project.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import rianbowgift.maven.maven1st_project.dto.summoner.SummonerDTO;
 
 
 @Controller
 public class HomeController {
 
 
-    @GetMapping("/")
-    public String home(Model model){
-        model.addAttribute("data","홈 화면");
+    @GetMapping("/")        //홈화면. 아이디를 입력받는다
+    public String home(){
         String SummonerName = "reimei"; //입력받아다 친다
         String api_key = "RGAPI-2cc0374f-4c93-47e5-9896-3bcb53653a3d"; //서버에 저장되있다 친다
-
-        return "homemain";
+        //
+        return "search";
     }
+
+    @PostMapping(value = "/search")
+    public String create(SummonerDTO summonerDTO) {
+        Member member = new Member();
+        member.setName(form.getName());
+        memberService.join(member);
+        return "redirect:/board?id=" + member.getName();
+    }
+
 
 }
---
+++ src/main/java/rianbowgift/maven/maven1st_project/service/SearchService.java
@@ -0,0 +1,6 @@
+package rianbowgift.maven.maven1st_project.service;
+
+public class SearchService {
+
+
+}
